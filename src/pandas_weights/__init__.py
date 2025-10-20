16dfb300609a5cc91b0dfe7574d1cb53ebc2e8ce
nachomaiz <ignacio.maiz@gmail.com>
2025-10-20 20:18:24 +0100

    explicit exports

--- src/pandas_weights/__init__.py
+++ src/pandas_weights/__init__.py
@@ -1,1 +1,6 @@
 from pandas_weights import frame as frame
+from pandas_weights import series as series
+from pandas_weights.frame import DataFrame
+from pandas_weights.series import Series
+
+__all__ = ("DataFrame", "Series", "frame", "series")
