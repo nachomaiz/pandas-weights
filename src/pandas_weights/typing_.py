0f43ca6f4d23391ad3ae4cb6dd23705f240342d5
nachomaiz <ignacio.maiz@gmail.com>
2025-10-29 11:27:49 +0000

    remove TypeAlias to support py3.9

--- src/pandas_weights/typing_.py
+++ src/pandas_weights/typing_.py
@@ -1,6 +1,6 @@
-from typing import TypeAlias, Union
+from typing import Union
 
 import numpy as np
 import pandas as pd
 
-D1NumericArray: TypeAlias = Union[list[Union[bool, int, float]], pd.Series, np.ndarray]
+D1NumericArray = Union[list[Union[bool, int, float]], pd.Series, np.ndarray]
